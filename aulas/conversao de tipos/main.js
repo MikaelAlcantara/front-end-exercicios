3131e93c80b8ecbfa9436a9e9bda18f893d19869
MikaelAlcantara <gundeloratao@gmail.com>
2025-09-15 19:17:33 -0300

    OI

---
+++ aulas/conversao de tipos/main.js
@@ -0,0 +1,43 @@
+//EXEMPLO 1
+
+
+//let numero = prompt ("Insira sua idade")
+
+
+//if (numero >10) {
+ 
+//console.log ("Parabéns seu numero é maior que 10")
+
+//} else {
+//    console.log ("Seu numero não é maior que 10")
+//} 
+
+
+//EXEMPLO 2
+
+
+//let numero = prompt ("Insira um numero")
+
+//if (numero >532 && numero <743 ) {
+//    console.log ("SEU NUMERO ESTÁ ENTRE OS CRITERIOS REQUISITADOS.")
+//} else {
+//    alert ("VOCE NAO ATENDE OS REQUISITOS ALUCINOGENOS")
+//}
+
+
+//EXEMPLO 3
+
+////let Mikael = 'Mikael123'
+//let Nostodos = 'aidento'
+
+
+//let login = prompt ("Insira sua senha")
+
+//if (login == Mikael || login == Nostodos) {
+//   console.log ("Está senha já está sendo utilizada por" + Mikael)
+//} else {
+//    alert ("Senha cadastrada com sucesso !")
+//}
+
+
+
