ad3a4834741c91b05ff157952259a47f2c15933f
55santoos <gundeloratao@gmail.com>
2025-09-24 21:49:20 -0300

    ATUALIZAR

--- exercicios-aluno/objetos/main.js
+++ exercicios-aluno/objetos/main.js
@@ -3,9 +3,9 @@ let livro = {
     autor:'Don Quixote',
     ano: 1990,
     ISBN: '1324657987422',
-    emprestar = 'Você pegou emprestado o Livro' + livro.titulo, 
-}
 
+}
+    emprestar : 'Você pegou emprestado o Livro' + livro.titulo, 
 
 
 console.log(livro.emprestar());
