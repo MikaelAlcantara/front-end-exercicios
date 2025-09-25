a29e3b60ecd1cb47c99ea68e6457896095e79fc0
55santoos <gundeloratao@gmail.com>
2025-09-24 21:47:38 -0300

    ATIVIDADE OBJETOS

---
+++ exercicios-aluno/objetos/main.js
@@ -0,0 +1,13 @@
+let livro = {
+    titulo: 'Don quixote de la mancha',
+    autor:'Don Quixote',
+    ano: 1990,
+    ISBN: '1324657987422',
+    emprestar = 'Você pegou emprestado o Livro' + livro.titulo, 
+}
+
+
+
+console.log(livro.emprestar());
+
+
