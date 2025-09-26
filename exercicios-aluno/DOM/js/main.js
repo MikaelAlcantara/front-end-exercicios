78fc8b1813148e3845aadcf6c3a39bbb32f3a83d
55santoos <gundeloratao@gmail.com>
2025-09-25 21:43:47 -0300

    ATUALIZAÇÃO DOS EXERCICIOS DOM

---
+++ exercicios-aluno/DOM/js/main.js
@@ -0,0 +1,17 @@
+// let livro = document.querySelector('.descricao-livro')
+// console.log (livro.innerText)
+
+// EXEMPLO 1
+
+// let livro = document.getElementById('o-cortico')
+// console.log(livro)
+
+// EXEMPLO 2
+
+// let ancora = document.getElementsByTagName('a');
+// console.log(ancora)
+
+// EXEMPLO 3
+
+// let ref = document.querySelector('#vidas-secas');
+// console.log(ref)
--- exercicios-aluno/objetos/main.js
+++ exercicios-aluno/objetos/main.js
@@ -8,4 +8,6 @@ var livro = {
     }
 }
 
+
+
 // window.prompt('oi');
